63efdf23922b1473475f7a895f3239401e807f75
Nici <contact@alternative-webdesign.fr>
2011-02-21 19:39:47 +0100

    Modif

--- aw/js/action.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.action').children('.act').css('visibility','hidden');
-    $('.action').mouseover(function(){
-        $('.action').children('.act').css('visibility','hidden');
-        $(this).children('.act').css('visibility','visible');
-    });
-});
